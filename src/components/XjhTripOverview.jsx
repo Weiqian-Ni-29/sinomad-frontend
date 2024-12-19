a04aadd4b1351bec08e841583264a9b249161c44
Weiqian-Ni-29 <weiqianni29@gmail.com>
2024-12-20 00:49:33 +0800

    update overview

--- src/components/XjhTripOverview.jsx
+++ src/components/XjhTripOverview.jsx
@@ -5,18 +5,18 @@ function XjhTripOverview() {
         <div className='outer-container'>
             <div className='overview'>
                 <h2>Overview</h2>
-                <p style={{lineHeight:'1.5em', fontSize: '1.05em'}}>Dive into the heart of Shanghai's former French Concession with our exclusive city walk tour, where East meets West in a vibrant cultural symphony.
-                     This intimate experience offers an unparalleled glimpse into the city's rich history and heritage. 
-                    Along the way, you'll explore dozens of protected historical buildings, each with a story waiting to be told. Our knowledgeable 
-                    guides will share the fascinating histories behind these architectural masterpieces, providing you with an in-depth experience of Shanghai's unique regional culture.
-                    Your day will begin at noon and conclude in the evening with a culinary experience that captures the essence of Shanghai. As part of our inclusive
-                    package, we provide water and a meal tailored to your dietary preferences.</p>
                 <p className="dot-before">Cultural Collision: Immerse in the fusion of Chinese and Western cultures in Shanghai's former French Concession.</p>
                 <p className="dot-before">Historical Landmarks: Journey past the former Far East's largest cathedral and one of China's most expensive Buddhist temples.</p>
                 <p className="dot-before">Architectural Tales: Discover dozens of protected historical buildings with intriguing, lesser-known stories.</p>
                 <p className="dot-before">Intimate Experience: Limited to 10 participants for a more personal and engaging tour.</p>
                 <p className="dot-before">Inclusive Amenities: Water and food included, with options to accommodate dietary preferences</p>
                 <p className="dot-before">Culinary Finale: Conclude the day with a taste of authentic Shanghai cuisine.</p>
+                <p style={{lineHeight:'1.5em', fontSize: '1.05em'}}>Dive into the heart of Shanghai's former French Concession with our exclusive city walk tour, where East meets West in a vibrant cultural symphony.
+                     This intimate experience offers an unparalleled glimpse into the city's rich history and heritage. 
+                    Along the way, you'll explore dozens of protected historical buildings, each with a story waiting to be told. Our knowledgeable 
+                    guides will share the fascinating histories behind these architectural masterpieces, providing you with an in-depth experience of Shanghai's unique regional culture.
+                    Your day will begin at noon and conclude in the evening with a culinary experience that captures the essence of Shanghai. As part of our inclusive
+                    package, we provide water and a meal tailored to your dietary preferences.</p>
             </div>
         </div>
         
