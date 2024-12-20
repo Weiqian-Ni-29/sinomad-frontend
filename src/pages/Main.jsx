6acd9d5422dadd085877429d1ede8f2ce82b7e56
Weiqian-Ni-29 <weiqianni29@gmail.com>
2024-12-20 17:59:39 +0800

    refactor about us to a separate page

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -4,9 +4,7 @@ import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
 import PortalImg from '../components/PortalImg';
 import Explore from '../components/Explore';
-// import ReviewSection from '../components/ReviewSection';
 import TravelGuide from '../components/TravelGuide';
-import AboutUs from '../components/AboutUs';
 
 function App() {
   return (
@@ -15,8 +13,6 @@ function App() {
         <PortalImg />
         <Explore />
         <TravelGuide />
-        {/* <ReviewSection /> */}
-        <AboutUs />
         <Footer />
     </div>
   );
