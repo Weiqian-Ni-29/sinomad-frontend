42adc1e2417c889e9a9a7c8aad4bf73bc3c9aa3a
Weiqian-Ni-29 <weiqianni29@gmail.com>
2024-12-25 18:42:14 +0800

    update banner image, aboutus section, and overall title and subtitle

--- src/components/PortalImg.jsx
+++ src/components/PortalImg.jsx
@@ -1,12 +1,12 @@
 import './PortalImg.css';
-import entranceImg from '../assets/imgs/chenghuangtempo-nightview-generated.webp'
+import entranceImg from '../assets/imgs/intro-banner.jpg'
 function PortalImg() {
     return (
         <div className="big-enterance-card">
             <img src={entranceImg} alt="entrance_card" />
             <div className="text-overlay">
-                <h2>Cheers to your Achievements</h2>
-                <p>We went, we reviewed, we reached milestones. See the big things we did together in 2024.</p>
+                <h2>Immersive Cultural Experience in China</h2>
+                <p>Providing genuine encounters that transcend mere sightseeing</p>
             </div>
         </div>
     );
