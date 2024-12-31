7a6768967d4de4a9232662db435d970a746397ea
Weiqian-Ni-29 <weiqianni29@gmail.com>
2024-12-31 13:55:27 +0800

    update route titles, add why choose us part, change display info on browser tab

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -5,12 +5,14 @@ import Footer from '../components/Footer';
 import PortalImg from '../components/PortalImg';
 import Explore from '../components/Explore';
 import TravelGuide from '../components/TravelGuide';
+import WhyChooseUs from '../components/WhyChooseUs';
 
 function App() {
   return (
     <div className="Main">
         <NavBar />
         <PortalImg />
+        <WhyChooseUs/>
         <Explore />
         <TravelGuide />
         <Footer />
