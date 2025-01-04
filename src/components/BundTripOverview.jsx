41889066dc2efa3f72608e9553a5122170757f86
Weiqian-Ni-29 <weiqianni29@gmail.com>
2025-01-04 18:43:08 +0800

    add xlb route

--- src/components/BundTripOverview.jsx
+++ src/components/BundTripOverview.jsx
@@ -5,15 +5,15 @@ function BundTripOverview() {
         <div className='outer-container'>
             <div className='overview'>
                 <h2>Overview</h2>
-                <p>Embark on an exclusive small-group photography walk from Suzhou Creek to the Bund, capturing hidden gems and history with an expericed local guide and a professional photographer, culminating in a traditional Shanghai hotpot.</p>
+                <p>Join a small-group photography walk from Suzhou Creek to the Bund, capturing hidden gems and landmarks like the Sihang Warehouse while exploring Shanghai's rich history. End the journey with a traditional Shanghai hotpot.</p>
                 <h2>Description</h2>
                 <p className="dot-before"><b>Intimate Photography Experience</b>: Limited to 6 participants for a personalized shooting experience.</p>
                 <p className="dot-before"><b>Day to Night Exploration</b>: Capture the essence of Shanghai from dawn to twilight, experiencing the fusion of history and modernity.</p>
-                <p className="dot-before"><b>Stunning Shoot Locations</b>: Discover off-the-beaten-path spots that are perfect for creating Instagram-worthy photos.</p>
+                <p className="dot-before"><b>Stunning Shoot Locations</b>: Discover off-the-beaten-path spots that are perfect for creating Instagram-worthy photos while learning the stories behind them.</p>
                 <p className="dot-before"><b>Inclusive Amenities</b>: Water and food included, with options to accommodate dietary preferences.</p>
                 <p className="dot-before"><b>Culinary Finale</b>: Conclude the day with a Shanghai-style hotpot .</p>
                 <p style={{lineHeight:'1.5em', fontSize: '1.05em'}}>Discover the magic of Shanghai's past and present on our exclusive 6-person photography walk from Suzhou Creek to the Bund. <br/>
-                    With our professional local guides and photographers, let's capture the city's allure at its best, from daylights to nightscapes, guaranteeing Instagram-worthy photos. <br/>
+                With our professional local guides and photographers, let's capture the city's allure at its best, from daylights to nightscapes, guaranteeing Instagram-worthy photos. Along the way, stop and shoot at iconic landmarks such as the Sihang Warehouse and more, while listening to captivating stories of Shanghai's development through the ages.<br/>
                     Your day will begin in the afernoon and will finish with a traditional hotpot as a perfect end to a day of urban exploration.As part of our inclusive package, we provide water and a meal tailored to your dietary preferences.</p>
             </div>
         </div>
--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import xujiahui_jingan_img from '../assets/imgs/jingantempo.jpg';
 import bund_img from '../assets/imgs/bund1.jpg';
 import jingze_img from '../assets/imgs/jingze1.jpg';
+import xlb_img from '../assets/imgs/xlb1.jpg';
 function Explore() {
   const navigate = useNavigate();
   // 点击时执行的跳转函数
@@ -19,17 +20,29 @@ function Explore() {
     event.stopPropagation();
     navigate('/Jingze');
   };
+  const handleClickXlb = (event) => {
+    event.stopPropagation();
+    navigate('/Xlb');
+  };
   return (
     <section id="experience">
       <div>
         <h2 className="explore-h2">Experience</h2>
         <div className="explore-cards">
-        <div className="explore-card">
+          <div className="explore-card">
             <div className="image-container">
               <img src={jingze_img} onClick={handleClickJingze} alt="jingze" />
             </div>
             <div className="card-description">
               <h4>Chinese Painting Experience by local master in Jiangnan Water Town</h4>
+            </div>
+          </div>
+          <div className="explore-card">
+            <div className="image-container">
+              <img src={xlb_img} onClick={handleClickXlb} alt="xlb" />
+            </div>
+            <div className="card-description">
+              <h4>Xiaolongbao Making in a Private Jiangnan Garden</h4>
             </div>
           </div>
           <div
