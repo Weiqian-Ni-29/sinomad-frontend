e7257a2bef3dc9d4a205194eb38b3bdd571fd4d5
Weiqian-Ni-29 <weiqianni29@gmail.com>
2025-03-29 21:29:03 +0800

    replace $ with ￥

--- src/pages/en/Jingze.jsx
+++ src/pages/en/Jingze.jsx
@@ -30,7 +30,7 @@ function Jingze(props) {
                     <Gallery imgs={imgs}/>
                 </div>
                 <div className="picker">
-                    <h3>From ${props.price} per person</h3>
+                    <h3>From ￥{props.price} per person</h3>
                     <h3>Select number of travellers and date of departure.</h3>
                     <TravelInfo price={props.price} route="jingze"/>
                     <div className="time-desc" >
--- src/pages/en/PaperCut.jsx
+++ src/pages/en/PaperCut.jsx
@@ -29,7 +29,7 @@ function PaperCut(props) {
                     <Gallery imgs={imgs}/>
                 </div>
                 <div className="picker">
-                    <h3>From ${props.price} per person</h3>
+                    <h3>From ￥{props.price} per person</h3>
                     <h3>Select number of travellers and date of departure.</h3>
                     <TravelInfo price={props.price} route="papercut"/>
                     <div className="time-desc">
